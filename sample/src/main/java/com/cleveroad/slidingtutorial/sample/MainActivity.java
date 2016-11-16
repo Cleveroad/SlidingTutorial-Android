d096fdebd90d4add227fe9004d471441d6f1ca87
ValeryGes <gecy.cr@gmail.com>
2016-11-16 16:28:25 +0200

    fix bug with view overlapping

--- sample/src/main/java/com/cleveroad/slidingtutorial/sample/MainActivity.java
+++ sample/src/main/java/com/cleveroad/slidingtutorial/sample/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 case 0: {
                     pageLayoutResId = R.layout.fragment_page_first;
                     tutorialItems = new TransformItem[]{
-                            TransformItem.create(R.id.ivFirstImage, Direction.LEFT_TO_RIGHT, 0.50f),
+                            TransformItem.create(R.id.ivFirstImage, Direction.LEFT_TO_RIGHT, 0.20f),
                             TransformItem.create(R.id.ivSecondImage, Direction.RIGHT_TO_LEFT, 0.06f),
                             TransformItem.create(R.id.ivThirdImage, Direction.LEFT_TO_RIGHT, 0.08f),
                             TransformItem.create(R.id.ivFourthImage, Direction.RIGHT_TO_LEFT, 0.1f),
@@ -100,7 +100,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 case 1: {
                     pageLayoutResId = R.layout.fragment_page_third;
                     tutorialItems = new TransformItem[]{
-                            TransformItem.create(R.id.ivFirstImage, Direction.RIGHT_TO_LEFT, 0.50f),
+                            TransformItem.create(R.id.ivFirstImage, Direction.RIGHT_TO_LEFT, 0.20f),
                             TransformItem.create(R.id.ivSecondImage, Direction.LEFT_TO_RIGHT, 0.06f),
                             TransformItem.create(R.id.ivThirdImage, Direction.RIGHT_TO_LEFT, 0.08f),
                             TransformItem.create(R.id.ivFourthImage, Direction.LEFT_TO_RIGHT, 0.1f),
@@ -113,7 +113,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 case 2: {
                     pageLayoutResId = R.layout.fragment_page_second;
                     tutorialItems = new TransformItem[]{
-                            TransformItem.create(R.id.ivFirstImage, Direction.RIGHT_TO_LEFT, 0.7f),
+                            TransformItem.create(R.id.ivFirstImage, Direction.RIGHT_TO_LEFT, 0.2f),
                             TransformItem.create(R.id.ivSecondImage, Direction.LEFT_TO_RIGHT, 0.06f),
                             TransformItem.create(R.id.ivThirdImage, Direction.RIGHT_TO_LEFT, 0.08f),
                             TransformItem.create(R.id.ivFourthImage, Direction.LEFT_TO_RIGHT, 0.1f),
